0f67cba58bcb249a0dfb3bb82b065df629058cdc
Pradeep Kays <pradeep.k@kaysharbor.com>
2026-08-08 23:38:49 +0530

    refactor: simplify Pest test setup by using 'uses' method

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,6 +5,4 @@ declare(strict_types=1);
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Pradeepdev\EnvironmentManager\Tests\TestCase;
 
-pest()->extend(TestCase::class)
-    ->use(RefreshDatabase::class)
-    ->in('Feature');
+uses(TestCase::class, RefreshDatabase::class)->in('Feature');
